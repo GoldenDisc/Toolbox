3e26a16288eb7e5ede70b270cc59346af1c21e2d
GoldenDisc <xavifmw@gmail.com>
2021-03-18 20:56:20 -0600

    Added Stacks.py

---
+++ DataStructures/Stacks.py
@@ -0,0 +1,29 @@
+
+class stack:
+
+    counter = -1
+
+    def __init__(self, arr=[]):
+
+        self.info = arr
+
+        if len(arr) > 0:
+
+            self.counter = len(arr) - 1
+            self.topValue = arr[-1]
+
+
+    def addToStack(self, value):
+
+        self.info.append(value)
+        self.counter += 1
+
+        self.topValue = self.info[self.counter]
+
+
+    def removeFromStack(self):
+
+        self.info.pop()
+        self.counter -= 1
+
+        self.topValue = self.info[self.counter]
